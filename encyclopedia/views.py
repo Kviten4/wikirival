f78500682bc85f4a5ca40211156883c08dcd3085
Kviten4 <thinking.art.hi@gmail.com>
2022-07-12 12:20:55 +0300

    tester's feedback

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from . import util
 import markdown
